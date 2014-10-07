9c300afcaaad4867816b08ab385a9cc05e04c2ea
AminHagos17 <40554@longroad.ac.uk>
2014-10-07 10:15:52 +0100

    selection exercises

---
+++ selection_revision_4.py
@@ -0,0 +1,24 @@
+#Amin Hagos
+#06-10-2014
+#Development Exercises Task 4
+
+grade = int(input("please enter your mark: "))
+
+if grade > 80:
+    print("you have achieved a grade A")
+
+elif grade > 70:
+    print("you have achieved a grade B")
+
+elif grade > 60:
+    print("you have achieved a grade C")
+
+elif grade > 50:
+    print("you have achieved a grade D")
+
+elif grade > 40:
+    print("you have achieved a grade E")
+
+else:
+    print("your grade is a U")
+    
---
+++ selection_stretch_challenge_2.py
@@ -0,0 +1,132 @@
+#Amin Hagos
+#06-10-2014
+#Stretch and Challenge Exercises Task 2
+
+print("the date you enter must be between 1st of January 2001 and 31st of December 2014") 
+day = int(input("please enter the day: "))
+month = int(input("please enter the month: "))
+year = int(input("please enter the year: "))
+
+if day == 1:
+    print("the day is the 1st")
+elif day == 2:
+    print("the day is the 2nd")
+elif day == 3:
+    print("the day is the 3rd")
+elif day == 4:
+    print("the day is the 4th")
+elif day == 5:
+    print("the day is the 5th")
+elif day == 6:
+    print("the day is the 6th")
+elif day == 7:
+    print("the day is the 7th")
+elif day == 8:
+    print("the day is the 8th")
+elif day == 9:
+    print("the day is the 9th")
+elif day == 10:
+    print("the day is the 10th")
+elif day == 11:
+    print("the day is the 11th")
+elif day == 12:
+    print("the day is the 12th")
+elif day == 13:
+    print("the day is the 13th")
+elif day == 14:
+    print("the day is the 14th")
+elif day == 15:
+    print("the day is the 15th")
+elif day == 16:
+    print("the day is the 16th")
+elif day == 17:
+    print("the day is the 17th")
+elif day == 18:
+    print("the day is the 18th")
+elif day == 19:
+    print("the day is the 19th")
+elif day == 20:
+    print("the day is the 20th")
+elif day == 21:
+    print("the day is the 21st")
+elif day == 22:
+    print("the day is the 22nd")
+elif day == 23:
+    print("the day is the 23rd")
+elif day == 24:
+    print("the day is the 24th")
+elif day == 25:
+    print("the day is the 25th")
+elif day == 26:
+    print("the day is the 26th")
+elif day == 27:
+    print("the day is the 27th")
+elif day == 28:
+    print("the day is the 28th")
+elif day == 29:
+    print("the day is the 29th")
+elif day == 30:
+    print("the day is the 30th")
+elif day == 31:
+    print("the day is the 31st")
+else:
+    print("*invalid day entered*")
+
+if month == 1:
+    print("of January,")
+elif month == 2:
+    print("of February,")
+elif month == 3:
+    print("of March,")
+elif month == 4:
+    print("of April,")
+elif month == 5:
+    print("of May,")
+elif month == 6:
+    print("of June,")
+elif month == 7:
+    print("of July,")
+elif month == 8:
+    print("of August,")
+elif month == 9:
+    print("of September,")
+elif month == 10:
+    print("of October,")
+elif month == 11:
+    print("of November,")
+elif month == 12:
+    print("of December,")
+else:
+    print("*invalid month entered*")
+
+
+if year == 1:
+    print("2001.")
+elif year == 2:
+    print("2002.")
+elif year == 3:
+    print("2003.")
+elif year == 4:
+    print("2004.")
+elif year == 5:
+    print("2005.")
+elif year == 6:
+    print("2006.")
+elif year == 7:
+    print("2007.")
+elif year == 8:
+    print("2008.")
+elif year == 9:
+    print("2009.")
+elif year == 10:
+    print("2010.")
+elif year == 11:
+    print("2011.")
+elif year == 12:
+    print("2012.")
+elif year == 13:
+    print("2013.")
+elif year == 14:
+    print("2014.")
+else:
+    print("*invalid year entered*")
